81a96a5441e4bcddfb8850bd0c1d0bacc940fb3d
Krayushkin68 <krayuskhinml97@gmail.com>
2021-10-26 17:56:43 +0300

    update

--- other/chrome_history/main.py
+++ other/chrome_history/main.py
@@ -14,18 +14,42 @@ def date_to_timestamp(date):
     return int(f'{dif.days * 60 * 60 * 24 + dif.seconds}{dif.microseconds:06d}')
 
 
+def check_url(con, cursor, url, visit_time):
+    cursor.execute("select id, visit_count, last_visit_time from urls where url = (?)", (url, ))
+    res = cursor.fetchone()
+    print(res)
+    if res:
+        id = res[0]
+        visit_count = int(res[1])
+        last_visit_time = res[2]
+        if timestamp_to_date(last_visit_time) < visit_time:
+            cursor.execute("update urls set visit_count = (?), last_visit_time = (?) where id = (?)",
+                           (visit_count+1, date_to_timestamp(visit_time), id))
+            con.commit()
+        else:
+            cursor.execute("update urls set visit_count = (?) where id = (?)",
+                           (visit_count + 1, id))
+            con.commit()
+        return id
+    else:
+        return False
+
+
 def add_history_row(url, visit_time):
     path_url = r'C:\Users\Krayu\AppData\Local\Google\Chrome\User Data\Default\History'
+    # path_url = r'data/History'
     try:
-        # Get url_id by url or create new
-        url_id = 38
-
         con = sqlite3.connect(path_url)
         con.execute("PRAGMA foreign_keys = 1")
         cursor = con.cursor()
-        query = 'insert into visits (url, visit_time, from_visit, transition, segment_id) values (?, ?, ?, ?, ?)'
-        cursor.execute(query, (url_id, date_to_timestamp(visit_time), 0, 805306368, 0))
-        con.commit()
+
+        # Get url_id by url or create new
+        url_id = check_url(con, cursor, url, visit_time)
+        if url_id:
+            query = 'insert into visits (url, visit_time, from_visit, transition, segment_id) values (?, ?, ?, ?, ?)'
+            cursor.execute(query, (url_id, date_to_timestamp(visit_time), 0, 805306368, 0))
+            con.commit()
+
         cursor.close()
         con.close()
     except sqlite3.Error as e:
@@ -54,6 +78,6 @@ def show_visits():
 
 
 if __name__ == '__main__':
-    date = datetime.datetime(2021, 10, 25, 18, 10, 13, 333)
-    add_history_row('url', date)
-    show_visits()
+    date = datetime.datetime(2021, 10, 26, 13, 48, 13, 333)
+    add_history_row('https://youtube.com/', date)
+    # show_visits()
