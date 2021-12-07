d3962e103eb3937e43b1cabd728b2c799c6f93b9
unknown <krayuskhinml97@gmail.com>
2021-12-07 20:26:51 +0300

    format changer

--- other/file_format_changer/main.py
+++ other/file_format_changer/main.py
@@ -1,6 +1,3 @@
-import pyparsing as pp
-
-
 def isfloat(number):
     try:
         float(number)
@@ -25,16 +22,43 @@ def reformat_file(input_filename, output_filename):
     data = data.splitlines()
     start_line = data[0]
     header = data[1].strip().split(',')
-    data = [[i.strip('"') for i in pp.commaSeparatedList.parseString(el.strip()).asList()] for el in data[2:]]
-    data = [[i.replace('.', ',') if isfloat(i) else i for i in el] for el in data]
+
+    data = data[2:]
+    parsed_data = []
+    for row in data:
+        start_idx = 0
+        row_elements = []
+        while True:
+            cur_idx = row.find(',', start_idx)
+            if cur_idx != -1:
+                if not row[start_idx:cur_idx].startswith('"'):
+                    el = row[start_idx:cur_idx].strip('"')
+                    el = el.replace('.', ',') if isfloat(el) else el
+                    row_elements.append(el)
+                    start_idx = cur_idx+1
+                else:
+                    quote_end_idx = row.find('",', start_idx)
+                    el = row[start_idx+1:quote_end_idx].strip('"')
+                    el = el.replace('.', ',') if isfloat(el) else el
+                    row_elements.append(el)
+                    start_idx = quote_end_idx+2
+            else:
+                el = row[start_idx:].strip('"')
+                el = el.replace('.', ',') if isfloat(el) else el
+                row_elements.append(el)
+                break
+        parsed_data.append(row_elements)
 
     with open(output_filename, 'wt', encoding='utf-8') as f:
         f.write(start_line+'\n')
         f.write('\t'.join(header)+'\n')
-        for el in data:
-            f.write('\t'.join(el))
-            f.write('\n')
+        for el in parsed_data:
+            f.write('\t'.join(el)+'\n')
 
 
 if __name__ == '__main__':
-    reformat_file('NeedsToBeConverted.txt', 'Output.txt')
+    reformat_file('From.txt', 'Output.txt')
+
+
+
+
